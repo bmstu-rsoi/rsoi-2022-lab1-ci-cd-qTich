be93d57f40d3e651455a42f7abcdbd613f248db6
Tichq <57302714+qTich@users.noreply.github.com>
2022-09-24 22:46:06 +0300

    allow: `Location` header if not set

--- src/person/views.py
+++ src/person/views.py
@@ -1,5 +1,7 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.renderers import JSONRenderer
+from rest_framework.settings import api_settings
+
 from .serializers import PersonSerializer
 from .models import PersonModel
 
@@ -8,6 +10,12 @@ class PersonAllAPIView(ListCreateAPIView):
     serializer_class = PersonSerializer
     queryset = PersonModel.objects.all()
     renderer_classes = [JSONRenderer]
+
+    def get_success_headers(self, data):
+        try:
+            return {'Location': str(data[api_settings.URL_FIELD_NAME])}
+        except (TypeError, KeyError):
+            return {'Location': f"/{data.get('id', '')}"}
 
 
 class PersonAPIView(RetrieveUpdateDestroyAPIView):
