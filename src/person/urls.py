f5e7c0b3b5ce3421605040add05a908ba8cc2d49
Tichq <57302714+qTich@users.noreply.github.com>
2022-09-24 18:36:52 +0300

    Initialization Django app.

---
+++ src/person/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from .views import PersonAPIView, PersonAllAPIView
+
+urlpatterns = [
+    path(
+        "persons/", PersonAllAPIView.as_view(),
+    ), path(
+        "persons/<int:pk>/", PersonAPIView.as_view(),
+    )
+]
